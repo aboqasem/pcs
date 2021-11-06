89cff2a008dcdf7d5eff5208131d89bd62fe16a4
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-11-06 18:42:45 +0800

    refactor(web/components): accept anchor tag's props in `Link`

--- apps/web/components/Link.tsx
+++ apps/web/components/Link.tsx
@@ -1,16 +1,47 @@
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
-import { memo, PropsWithChildren, useMemo } from 'react';
+import { HTMLProps, memo, PropsWithChildren, useMemo } from 'react';
 
-export interface ILinkProps extends PropsWithChildren<NextLinkProps> {
+export interface ILinkProps
+  extends PropsWithChildren<
+    Omit<NextLinkProps, 'passHref'> &
+      Omit<HTMLProps<HTMLAnchorElement>, keyof NextLinkProps | 'className'>
+  > {
   className?: string;
 }
 
-export const Link = memo(function Link({ className, children, ...linkProps }: ILinkProps) {
-  const a = useMemo(() => <a className={className}>{children}</a>, [children, className]);
+export const Link = memo(function Link({
+  className,
+  children,
+  href,
+  as,
+  replace,
+  scroll,
+  shallow,
+  prefetch,
+  locale,
+  ...anchorProps
+}: ILinkProps) {
+  const anchor = useMemo(
+    () => (
+      <a className={className} {...anchorProps}>
+        {children}
+      </a>
+    ),
+    [anchorProps, children, className],
+  );
 
   return (
-    <NextLink {...linkProps} passHref>
-      {a}
+    <NextLink
+      href={href}
+      as={as}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      prefetch={prefetch}
+      locale={locale}
+      passHref
+    >
+      {anchor}
     </NextLink>
   );
 });
