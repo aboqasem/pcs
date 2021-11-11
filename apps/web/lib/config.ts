7b7fce8c756edabb9bfe93ff2b24c4bcbe88ae13
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-11-11 08:58:32 +0800

    refactor(web): `staleTime` and `cacheTime` as environment vars

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -4,6 +4,10 @@ class Configurations {
   readonly NODE_ENV: string = process.env.NODE_ENV || NodeEnv.Development;
 
   readonly BFF_URL = process.env.NEXT_PUBLIC_BFF_URL || 'http://localhost:8000/api/';
+
+  readonly RQ_STALE_TIME = +(process.env.NEXT_PUBLIC_RQ_CACHE_TIME ?? 60 * 1000 * 8); // 8 minutes default
+
+  readonly RQ_CACHE_TIME = +(process.env.NEXT_PUBLIC_RQ_STALE_TIME ?? 60 * 1000 * 10); // 10 minutes default
 }
 
 export const config = new Configurations();
